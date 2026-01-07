073f320768f913b64a044130e1c0cfa71eb91531
202378 <smfwn531@hs.ac.kr>
2026-01-08 01:39:18 +0900

    edit

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -1,7 +1,21 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
-class Task(BaseModel):
-    id: int
-    title: Optional[str] = Field(None, example="세탁소에 맡긴 것을 찾으러 가기")
-    done: bool = Field(False, description="완료 플래그")
+class TaskBase(BaseModel):
+    title : Optional[str] = Field(None, example="간장 사기")
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id : int
+
+    class config:
+        orm_mode = True
+
+class Task(TaskBase):
+    id : int
+    done : bool = Field(False, description="완료 플래그")
+    
+    class Config :
+        orm_mode = True
